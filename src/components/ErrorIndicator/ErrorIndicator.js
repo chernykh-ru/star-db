7451624ab6a1ec6769b5e1243fc738beb6e3d049
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-01-31 16:20:56 +0300

    added error handling

---
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ErrorIndicator.css';
+import errorImage from './death-star.png';
+const ErrorIndicator = () => {
+  return (
+    <div className='error-indicator'>
+      <img src={errorImage} alt='error icon' />
+      <span className='boom'>BOOM!</span>
+      <span>something has gone terribly rong</span>
+      <span>(but we already sent droids to fix it)</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
